e674076f0aeecb9dfbd40a5f2510f8139aae6295
Lidiane Faria <lidianerdefaria@gmail.com>
2021-08-01 21:43:32 -0300

    tecga

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$('.autoplay').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  dots: false,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+  ]
+})
